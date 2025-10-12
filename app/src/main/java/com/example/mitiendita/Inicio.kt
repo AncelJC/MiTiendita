3bd7b700e8cfeaff71b696d1239f5e8cd23da0b9
Ancel JC <anceljulcamoro@gmail.com>
2025-10-11 23:56:26 -0500

    Actualizacion

--- app/src/main/java/com/example/mitiendita/Inicio.kt
+++ app/src/main/java/com/example/mitiendita/Inicio.kt
@@ -1,12 +1,14 @@
 package com.example.mitiendita
 
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.mitiendita.ui.CarritoFragment
 import com.example.mitiendita.ui.CategoriaFragment
 import com.example.mitiendita.ui.InicioFragment
@@ -15,43 +17,60 @@ import com.google.android.material.navigation.NavigationView
 class Inicio : AppCompatActivity() {
 
     private lateinit var dlayMenu: DrawerLayout
-    private lateinit var navMenu: NavigationView
+    private lateinit var nvMenu: NavigationView
     private lateinit var ivMenu: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_inicio)
 
         dlayMenu = findViewById(R.id.dlayMenu)
-        navMenu = findViewById(R.id.navMenu)
+        nvMenu = findViewById(R.id.nvMenu)
         ivMenu = findViewById(R.id.ivMenu)
+
 
         ivMenu.setOnClickListener {
             dlayMenu.open()
         }
 
-        navMenu.setNavigationItemSelectedListener{ menuItem ->
+        nvMenu.setNavigationItemSelectedListener{ menuItem ->
             menuItem.isChecked = true // Define que se seleccione el item
             dlayMenu.closeDrawers()//Cierra el menu desplegable
 
-            //Maneja las selecciones
+//            Maneja las selecciones
 
             when (menuItem.itemId) {
                 R.id.itInicio -> replaceFragment(InicioFragment())
                 R.id.itCategoria -> replaceFragment(CategoriaFragment())
                 R.id.itCarrito -> replaceFragment(CarritoFragment())
-                R.id.itPerfil -> replaceFragment(PerfilFragment())
-                else -> false
+//                R.id.itPerfil -> replaceFragment(PerfilFragment())
+
             }
+            true
         }
 
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        //Hace que el teclado del dispositivo no tape a las Views (EditText, TextImputEditText, etc)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dlayMenu)) { v, insets ->
+            val imeInsets = insets.getInsets(WindowInsetsCompat.Type.ime())
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                maxOf(systemBars.bottom, imeInsets.bottom)
+                )
             insets
         }
+        replaceFragment(InicioFragment())
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.contenedorFragment, fragment)
+            .commit()
     }
 }
